e2bae3accca519886e59252e062f81f4c0dca218
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-28 15:08:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
   end
 
   def genres
-    Song.all.select{|song| song.artist == self}.collect{|song| song.genre}
+    Song.all.select{|song| song.artist == self}.collect{|song| song.genre}.uniq
   end
 
 
